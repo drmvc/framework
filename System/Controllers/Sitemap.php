3fc8997e54ddcf23a64f4108cc2192c4c15ccb3e
Pavel Rykov <p.rykov@drteam.rocks>
2016-06-22 03:34:41 +0300

    sitemap pages array bugfix

--- System/Controllers/Sitemap.php
+++ System/Controllers/Sitemap.php
@@ -24,14 +24,15 @@ class Sitemap extends Controller
         $appnamespace = '\\Application\\Controllers\\';
         $sysnamespace = '\\System\\Controllers\\';
 
-        $appfiles = glob(DOCROOT . 'Application/Controllers/*.php');
+        $appfiles = glob(APPPATH . '/Controllers/*.php');
         //$sysfiles = glob(DOCROOT . 'System/Controllers/*.php');
         $sysfiles = array();
         $files = array_merge($appfiles, $sysfiles);
 
         $classes = array();
         $i = 0;
-        foreach ($files as $file) {
+        while ($i < count($files)) {
+            $file = $files[$i];
             $segments = explode('/', $file);
             $segments = explode('\\', $segments[count($segments) - 1]);
             $filename = $segments[count($segments) - 1];
@@ -41,24 +42,30 @@ class Sitemap extends Controller
             $sysclass = $sysnamespace . $classname[0];
 
             switch (true) {
-                case (file_exists(DOCROOT . 'Application/Controllers/' . $classname[0] . '.php')):
+                case (file_exists(APPPATH . '/Controllers/' . $classname[0] . '.php')):
                     $class_name = $appnamespace . $classname[0];
-                    $class_check = new $appclass();
+                    $class_check = $appclass;
                     break;
-                case (file_exists(DOCROOT . 'System/Controllers/' . $classname[0] . '.php')):
+                case (file_exists(SYSPATH . '/Controllers/' . $classname[0] . '.php')):
                     $class_name = $sysnamespace . $classname[0];
-                    $class_check = new $sysclass();
+                    $class_check = $sysclass;
                     break;
                 default:
-                    $class_name = NULL;
-                    $class_check = NULL;
+                    $class_name = null;
+                    $class_check = false;
                     break;
             }
 
             $classes[$i]['name'] = $class_name;
             $classes[$i]['name_short'] = $classname[0];
             $classes[$i]['path'] = $file;
-            $classes[$i]['methods'] = get_class_methods($class_check);
+
+            $methods = get_class_methods($class_check);
+            $m = 0;
+            while ($m < count($methods)) {
+                if (preg_match('#^action_#i', $methods[$m]) === 1) $classes[$i]['methods'][] = $methods[$m];
+                $m++;
+            }
 
             $i++;
         }
